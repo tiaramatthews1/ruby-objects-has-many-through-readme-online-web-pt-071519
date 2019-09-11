4ce047b7bc0cf1bba306575387dc9c554ea00305
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-09-11 14:39:27 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -16,10 +16,10 @@ class Customer
   Meal.all.select do |meal|
     meal.customer == self
   end
+end
    def new_meal(waiter, total, tip=0)
     Meal.new(waiter, self, total, tip)
   end
-end
 def waiters
   meals.map do |meal|
     meal.waiter
